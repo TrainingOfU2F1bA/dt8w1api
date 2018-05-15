41c4ecafc33442510968880ae5cb2f7a720eea19
saul <eternx@163.com>
2018-05-15 16:35:47 +0800

    <003> refactor((Company)|(Employee)Controller):add mapping methods

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -1,5 +1,58 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Company;
+import com.example.employee.restfulapi.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController(CompanyController.COMPANIES)
 public class CompanyController {
-    //在此处完成Company API
+
+    public static final String COMPANIES = "companies";
+    public static final String COMPANIES_ID_EMPLOYEES = "companies/{id:\\d*}/employees";
+    public static final String COMPANIES_ID = "companies/{id:\\d*}";
+    public static final String COMPANIES_PAGE_PAGE_NUM_PAGE_SIZE_PAGE_SIZE = "companies/page/{pageNum}/pageSize/{pageSize}";
+
+    @GetMapping(COMPANIES_ID_EMPLOYEES)
+    public List<Employee> listEmployees(Long id) {
+        return null;
+    }
+
+
+    @GetMapping
+    public List<Company> list() {
+        return null;
+    }
+
+
+    @DeleteMapping(COMPANIES_ID)
+    public void delete(Long id) {
+
+    }
+
+
+    @GetMapping(COMPANIES_ID)
+    public Company find(Long id) {
+        return null;
+    }
+
+
+    @GetMapping(COMPANIES_PAGE_PAGE_NUM_PAGE_SIZE_PAGE_SIZE)
+    public Page<Company> pages(int pageNum, int pageSize) {
+        return null;
+    }
+
+
+    @PutMapping
+    public void update(@RequestBody Company company) {
+
+    }
+
+
+    @PostMapping
+    public void add(@RequestBody Company company) {
+
+    }
 }
--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -1,5 +1,57 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Employee;
+import com.example.employee.restfulapi.service.EmployeeService;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController(EmployeeController.EMPLOYEES)
 public class EmployeeController {
-    //在此处完成Employee API
+
+    public static final String EMPLOYEES = "employees";
+    public static final String PAGE_PAGE_NUM_PAGE_SIZE_PAGE_SIZE = "employees/page/{pageNum:\\d*}/pageSize/{pageSize:\\d*}";
+    public static final String ID = "employees/{id:\\d*}";
+    public static final String GENDER = "employees/{gender:female|male}";
+
+    @GetMapping(GENDER)
+    public List<Employee> listWithGender(String gender) {
+        return null;
+    }
+
+
+    @GetMapping
+    public List<Employee> list() {
+        return null;
+    }
+
+
+    @DeleteMapping(ID)
+    public void delete(Long id) {
+
+    }
+
+
+    @GetMapping(ID)
+    public Employee find(Long id) {
+        return null;
+    }
+
+
+    @GetMapping(PAGE_PAGE_NUM_PAGE_SIZE_PAGE_SIZE)
+    public Page<Employee> pages(int pageNum, int pageSize) {
+        return null;
+    }
+
+
+    @PutMapping
+    public void update(@RequestBody Employee employee) {
+
+    }
+
+    @PostMapping
+    public void add(@RequestBody Employee employee) {
+
+    }
 }
