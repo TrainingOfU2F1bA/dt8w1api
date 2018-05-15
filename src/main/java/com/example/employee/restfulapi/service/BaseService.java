65fa0fb00208c072a3d61f5abfc4aaca6ca42e80
saul <eternx@163.com>
2018-05-15 16:39:59 +0800

    fix(BaseService):fix a generic problem

--- src/main/java/com/example/employee/restfulapi/service/BaseService.java
+++ src/main/java/com/example/employee/restfulapi/service/BaseService.java
@@ -10,6 +10,6 @@ public interface BaseService<T> {
     public void delete(Long id);
     public T find(Long id);
     public Page<T> pages(int pageNum,int pagesize);
-    public void update(Employee employee);
-    public void add(Employee employee);
+    public void update(T employee);
+    public void add(T employee);
 }
